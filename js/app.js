e3908be7e7a1608c2e55a2e8c02b4fe4d86661fd
Lauren Murphy <Lmurphy@gmx.com>
2022-01-31 16:50:24 -0800

    Added core constructors and setup for save data

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+
 // This is how many plot objects will be instantiated to populate the plotGrid
 const plotQty = 25;
 
@@ -7,19 +9,28 @@ const plotQty = 25;
 
 let plotGrid = document.getElementById('plotGrid');
 let inventoryGrid = document.getElementById('inventoryGrid');
-let cropTypes = [];
+
+//Will not be saved, created at initialization:
+let cropTypes = []; //List of all possible crop types implemented
+let allItems = []; //List of all possible items the player could have in their inventory
+
+//Will be saved/loaded:
+let playerInventory = []; //list of slugs
+let playerMoney = 0;
+let plotGridState = []; //list of LivePlant objects and empty spaces
 
 function initPlotGrid() {
-// initial render of blank plots, aka populate DOM with plot elements and create an array of plot elements
-  for (let i = 0; i < plotQty; i++){
+  // initial render of blank plots, aka populate DOM with plot elements and create an array of plot elements
+  for (let i = 0; i < plotQty; i++) {
     let newPlot = document.createElement('div');
-    newPlot.setAttribute('id',`plot-${i}`);
-    newPlot.setAttribute('class','plot-div');
+    newPlot.setAttribute('id', `plot-${i}`);
+    newPlot.setAttribute('class', 'plot-div');
     plotGrid.appendChild(newPlot);
   }
 }
 
-function Crop(yieldQty,sellValue,growthTime,sprites,slug){
+// Constructor for 
+function Crop(yieldQty, sellValue, growthTime, sprites, slug) {
   this.yieldQty = yieldQty,
   this.sellValue = sellValue;
   this.growthTime = growthTime;
@@ -28,19 +39,40 @@ function Crop(yieldQty,sellValue,growthTime,sprites,slug){
   cropTypes.push(this);
 }
 
-let corn = new Crop(3,200,30,[],'corn');
-
-function LivePlant(cropSlug,age = 0,needsWater = false){
+function LivePlant(cropSlug, age = 0, needsWater = false, location) {
   this.cropSlug = cropSlug,
   this.age = age,
   this.needsWater = needsWater;
+  this.location = location; //the index number of the plot space
 }
 
-PlotContents
+//Function called when player sows seeds
+function SowSeedAtLocation(location, seedType){
+  plotGridState[location] = new LivePlant(seedType, 0, false, location);
+  //save the plotgridstate
+}
 
+
+//Items appear in the inventory.  For now only Seeds are items
+function Item(slug, title, sprite) {
+  this.slug = slug;
+  this.title = title;
+  this.sprite = sprite;
+  allItems.push(this);
+}
+
+//Tracks longterm user stats, used on the stats page and saved to localStorage
+function UserStats(){
+  this.totalPlayTime;
+  this.totalMoneyGained;
+}
 
 // function saveGame{
 
 // }
+
+let corn = new Crop(3, 200, 30, [], 'corn');
+
+let cornSeeds = new Item('cornseeds','Corn Seeds', 'cornseeds');
 
 initPlotGrid();
